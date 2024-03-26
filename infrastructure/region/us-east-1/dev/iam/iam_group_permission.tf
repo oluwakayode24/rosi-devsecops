d9938350584cd41de4d754a7338ab05463d5ea1e
Oluwakayode <odetundeoluwakayode@gmail.com>
2024-03-26 11:56:09 +0000

    rosi infrastructure changes

---
+++ infrastructure/region/us-east-1/dev/iam/iam_group_permission.tf
@@ -0,0 +1,26 @@
+module "rosi" {
+  source = "../../../../..//modules/iam"
+
+  group_name         = "rosi"
+  policy_name        = "rosi-permission"
+  policy_description = "this is a limited permission for rosi users"
+  managed_policies_to_attach = [
+    "arn:aws:iam::aws:policy/AmazonS3FullAccess",
+    "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
+  ]
+  inline_policies_to_attach = data.aws_iam_policy_document.rosi_permission.json
+}
+
+
+data "aws_iam_policy_document" "rosi_permission" {
+  statement {
+    sid = "1"
+
+    actions = [
+      "kms:Encrypt"
+    ]
+    resources = [
+      "*",
+    ]
+  }
+}
