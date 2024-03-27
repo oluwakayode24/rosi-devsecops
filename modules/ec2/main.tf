a92c65ad6416e5f36143d5137200daaf74164903
Oluwakayode <odetundeoluwakayode@gmail.com>
2024-03-27 17:10:17 +0000

    Github Actions workflow

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "rosi_instance_module" {
-  ami                     = var.aws_ami
-  instance_type           = var.instance_type
-  availability_zone       = var.availability_zone
-  tags                    = var.tags
-  #region                  = var.region
+  ami               = var.aws_ami
+  instance_type     = var.instance_type
+  availability_zone = var.availability_zone
+  tags              = var.tags
+  #region           = var.region
 }
