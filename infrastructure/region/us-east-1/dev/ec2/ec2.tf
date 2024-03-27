81a041121022b6846b4d92145a8e72982c11c22e
Oluwakayode <odetundeoluwakayode@gmail.com>
2024-03-27 19:07:41 +0000

    latest changes

--- infrastructure/region/us-east-1/dev/ec2/ec2.tf
+++ infrastructure/region/us-east-1/dev/ec2/ec2.tf
@@ -5,7 +5,7 @@ module "ec2_instance" {
   instance_type     = "t2.micro"
   availability_zone = "us-east-1a"
   tags = {
-    Name = "rosi_ec2_instance" #aws instance name tag
+    Name = "rosi_ec2_instance"
   }
   region = "us-east-1"
 }
